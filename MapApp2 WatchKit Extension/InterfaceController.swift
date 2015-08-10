c50b7171cd314c8dea59f1f1375635177bb09715
Maciej Stanik <M.Stanik@kainos.com>
2015-08-10 13:09:15 +0200

    zoom slider added

--- MapApp2 WatchKit Extension/InterfaceController.swift
+++ MapApp2 WatchKit Extension/InterfaceController.swift
@@ -11,33 +11,37 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
     @IBOutlet weak var map: WKInterfaceMap!
+    
+    let location = CLLocationCoordinate2D(latitude: 54.444321, longitude: 18.56)
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        
-        let location = CLLocationCoordinate2D(latitude: 54.444321, longitude: 18.56)
-        
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-        
         let region = MKCoordinateRegion(center: location, span: span)
         
         map.setRegion(region)
-        
         map.addAnnotation(location, withPinColor: WKInterfaceMapPinColor.Green)
         
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    @IBAction func sliderValueChanged(value: Float) {
+        let degrees:CLLocationDegrees = CLLocationDegrees(10 - value) / 300
+        let span = MKCoordinateSpanMake(degrees, degrees)
+        let region = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region)
+    }
 }
